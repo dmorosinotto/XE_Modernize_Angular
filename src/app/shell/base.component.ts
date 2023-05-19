7059af39649955e5dd8254192da5f7fa6598b361
Daniele Morosinotto <d.morosinotto@icloud.com>
2023-05-19 18:33:13 +0200

    4.9 DestoryRef -> takeUntilDestory

--- src/app/shell/base.component.ts
+++ src/app/shell/base.component.ts
@@ -1,19 +1,19 @@
-import { Directive, OnDestroy } from "@angular/core";
+import { Directive, DestroyRef, inject } from "@angular/core";
 import { Subject } from "rxjs";
 import { takeUntil } from "rxjs/operators";
 
+const injectTakeUntilDestroy = () => {
+	console.info("ADESSO CREO _destory$");
+	const _destroy$ = new Subject<void>();
+	inject(DestroyRef).onDestroy(() => {
+		console.info("EQUIVALE A ngOnDestory");
+		_destroy$.next();
+		_destroy$.complete();
+	});
+	return takeUntil<any>(_destroy$);
+};
+
 @Directive()
-export abstract class BaseComponent implements OnDestroy {
-	// _destroy$ is LAZY: it'll be created (and allocate memory) only if you use takeUntilDestroy
-	private _destroy$?: Subject<void>;
-	protected takeUntilDestroy = <T>() => {
-		if (!this._destroy$) this._destroy$ = new Subject<void>(); // LAZY Subject
-		return takeUntil<T>(this._destroy$);
-	};
-	ngOnDestroy() {
-		if (this._destroy$) {
-			this._destroy$.next();
-			this._destroy$.complete();
-		}
-	}
+export abstract class BaseComponent {
+	protected takeUntilDestroy = injectTakeUntilDestroy;
 }
