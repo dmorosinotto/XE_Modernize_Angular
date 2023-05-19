8c077a994e2dac96749c1f211d7b5548650cce29
Daniele Morosinotto <d.morosinotto@icloud.com>
2023-05-19 10:20:13 +0200

    2.0 provideHttp + InterceptorFn

--- src/app/state/auth/auth.interceptor.ts
+++ src/app/state/auth/auth.interceptor.ts
@@ -1,15 +1,9 @@
-import { HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from "@angular/common/http";
-import { Injectable } from "@angular/core";
+import { HttpInterceptorFn } from "@angular/common/http";
+import { inject } from "@angular/core";
 import { AuthService } from "./auth.service";
 
-@Injectable()
-export class AuthInterceptor implements HttpInterceptor {
-	constructor(private auth: AuthService) {}
-
-	intercept(req: HttpRequest<any>, next: HttpHandler) {
-		// send cloned request with Authorization Header to the next handler.
-		return next.handle(this.auth.addAuthorizationHeader(req));
-	}
-}
-
-export const httpInterceptorProviders = [{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }];
+export const authInterceptor: HttpInterceptorFn = (req, next) => {
+	const auth = inject(AuthService); //USO inject PER DI!!
+	// send cloned request with Authorization Header to the next handler.
+	return next(auth.addAuthorizationHeader(req));
+};
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,15 @@
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
-
 import { importProvidersFrom } from "@angular/core";
 import { AppComponent } from "./app/app.component";
 import { AppRoutingModule } from "./app/app.route";
-import { withInterceptorsFromDi, provideHttpClient } from "@angular/common/http";
+import { withInterceptors, provideHttpClient } from "@angular/common/http";
 import { BrowserModule, bootstrapApplication } from "@angular/platform-browser";
-import { httpInterceptorProviders } from "@app/state/auth";
+import { authInterceptor } from "@app/state/auth";
 
 bootstrapApplication(AppComponent, {
-    providers: [
-        importProvidersFrom(BrowserModule, AppRoutingModule),
-        httpInterceptorProviders,
-        provideHttpClient(withInterceptorsFromDi())
-    ]
-})
-	.catch(err => console.error(err));
+	providers: [
+		importProvidersFrom(BrowserModule, AppRoutingModule),
+		provideHttpClient(withInterceptors([authInterceptor]))
+	]
+}).catch(err => console.error(err));
