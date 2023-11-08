051ebc17a540c85fca60367bb17b0bd5167b5ee4
Daniele Morosinotto <d.morosinotto@icloud.com>
2023-11-08 17:39:02 +0100

    (chore) add .prittierrc + NG17 product

--- src/app/state/product.service.ts
+++ src/app/state/product.service.ts
@@ -1,55 +1,61 @@
 import { InjectionToken, Injectable, inject, makeEnvironmentProviders } from "@angular/core";
 
 export interface Product {
-	id: number;
-	name: string;
-	price: number;
-	description: string;
+    id: number;
+    name: string;
+    price: number;
+    description: string;
 }
 
 const FAKE_PRODUCTS: Product[] = [
-	{
-		id: 16,
-		name: "NG 16",
-		price: 0.01,
-		description: "Lastest and gratest with Signal preview"
-	},
-	{
-		id: 15,
-		name: "NG 15",
-		price: 2.9,
-		description: "Stable standalone + provide API"
-	},
-	{
-		id: 14,
-		name: "NG 14",
-		price: 3.0,
-		description: "Introducing standalone + inject"
-	},
-	{
-		id: 13,
-		name: "NG 9-13",
-		price: 2.7,
-		description: ""
-	}
+    {
+        id: 17,
+        name: "NG 17",
+        price: 0.811,
+        description: "SIGNAL + @-SYNTAX + NEW logo, docs and evrything good ^_^",
+    },
+    {
+        id: 16,
+        name: "NG 16",
+        price: 0.01,
+        description: "Lastest and gratest with Signal preview",
+    },
+    {
+        id: 15,
+        name: "NG 15",
+        price: 2.9,
+        description: "Stable standalone + provide API",
+    },
+    {
+        id: 14,
+        name: "NG 14",
+        price: 3.0,
+        description: "Introducing standalone + inject",
+    },
+    {
+        id: 13,
+        name: "NG 9-13",
+        price: 2.7,
+        description: "",
+    },
 ];
 
 export const PRICE_LIMIT = new InjectionToken<number>(
-	"PRICE_LIMIT"
-	/*, { providedIn: "root", factory: () => 700 }*/
+    "PRICE_LIMIT"
+    /*, { providedIn: "root", factory: () => 700 }*/
 );
 
 @Injectable(/*{ providedIn: "root" }*/)
 export class ProductService {
-	public LIMIT = inject(PRICE_LIMIT); //automatic infer :number
+    public LIMIT = inject(PRICE_LIMIT); //automatic infer :number
 
-	public getAll() {
-		return FAKE_PRODUCTS;
-	}
+    public getAll() {
+        return FAKE_PRODUCTS;
+    }
 }
 
 export const provideProductService = (config: { limit: number }) =>
-	makeEnvironmentProviders([
-		{ provide: PRICE_LIMIT, useValue: config.limit },
-		{ provide: ProductService, useClass: ProductService }
-	]);
+    makeEnvironmentProviders([
+        { provide: PRICE_LIMIT, useValue: config.limit },
+        { provide: ProductService, useClass: ProductService },
+    ]);
