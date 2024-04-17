67fe9e2347fe17aa693209f06904669c4a2b311b
Daniele Morosinotto <d.morosinotto@icloud.com>
2024-04-17 15:55:08 +0200

    using input, input.required and output

--- src/app/products/product-alerts/product-alerts.component.ts
+++ src/app/products/product-alerts/product-alerts.component.ts
@@ -1,16 +1,21 @@
-import { Component, EventEmitter, Input, Output, OnInit } from "@angular/core";
+import { Component, EventEmitter, input, output, OnInit, computed } from "@angular/core";
 import { Product } from "@app/state/product.service";
 import { NgIf } from "@angular/common";
 
 @Component({
-	selector: "app-product-alerts",
-	templateUrl: "./product-alerts.component.html",
-	styles: [":host { display: inline-flex }"],
-	standalone: true,
-	imports: [NgIf]
+    selector: "app-product-alerts",
+    templateUrl: "./product-alerts.component.html",
+    styles: [":host { display: inline-flex }"],
+    standalone: true,
+    imports: [NgIf],
 })
 export class ProductAlertsComponent {
-	@Input() product?: Product;
-	@Output() notify = new EventEmitter<number>();
-	@Input({ required: true }) limit!: number; //ASSERTION SU OnINIT NON PIU NECESSARIA
+    product = input<Product>();
+    notify = output<number>();
+    limit = input.required<number>(); //ASSERTION SU OnINIT NON PIU NECESSARIA
+    showNotifyButton = computed(() => {
+        const product = this.product();
+        if (!product) return false; //null or undefined
+        return product.price < this.limit();
+    });
 }
