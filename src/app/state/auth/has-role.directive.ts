4a0866c5d5309392b7ea48b6ce7691293c47e591
Daniele Morosinotto <d.morosinotto@icloud.com>
2024-04-17 15:51:49 +0200

    effect best-practice: use untracked + explicit signal deps

--- src/app/state/auth/has-role.directive.ts
+++ src/app/state/auth/has-role.directive.ts
@@ -1,40 +1,42 @@
-import { Directive, EmbeddedViewRef, Input, TemplateRef, ViewContainerRef, effect, Injector } from "@angular/core";
+import { Directive, EmbeddedViewRef, Input, TemplateRef, ViewContainerRef, effect, Injector, untracked } from "@angular/core";
 import { AuthService } from "./auth.service";
 
 @Directive({
-	selector: "[hasRole]",
-	standalone: true
+    selector: "[hasRole]",
+    standalone: true,
 })
 export class HasRoleDirective {
-	constructor(
-		private _injector: Injector,
-		private auth: AuthService,
-		private _templateRef: TemplateRef<any>,
-		private _viewContainer: ViewContainerRef
-	) {}
+    constructor(
+        private _injector: Injector,
+        private auth: AuthService,
+        private _templateRef: TemplateRef<any>,
+        private _viewContainer: ViewContainerRef
+    ) {}
 
-	private _role?: string;
-	@Input() public set hasRole(role: string) {
-		if (this._role !== role) {
-			this._role = role;
-			console.warn("CREATE NEW computed + effect");
-			const computedHasRole = this.auth.hasRole(role);
-			effect(
-				() => {
-					let visible = computedHasRole();
-					console.log("NOW VISIBLE", visible, " TO ", role);
-					if (visible) this._viewRef = this._viewContainer.createEmbeddedView(this._templateRef);
-					else if (this._viewRef) {
-						this._viewContainer.clear();
-						this._viewRef = undefined;
-					}
-				},
-				{ injector: this._injector }
-			);
-		}
-	}
+    private _role?: string;
+    @Input() public set hasRole(role: string) {
+        if (this._role !== role) {
+            this._role = role;
+            console.warn("CREATE NEW computed + effect");
+            const computedHasRole = this.auth.hasRole(role);
+            effect(
+                () => {
+                    let visible = computedHasRole();
+                    console.log("NOW VISIBLE", visible, " TO ", role);
+                    untracked(() => {
+                        if (visible) this._viewRef = this._viewContainer.createEmbeddedView(this._templateRef);
+                        else if (this._viewRef) {
+                            this._viewContainer.clear();
+                            this._viewRef = undefined;
+                        }
+                    });
+                },
+                { injector: this._injector }
+            );
+        }
+    }
 
-	private _viewRef?: EmbeddedViewRef<any>;
+    private _viewRef?: EmbeddedViewRef<any>;
 }
 
 //SAMPLE SCAM DIRECTIVE
