c4a703bf5ab069c5b38818b550bb4f360e390884
Daniele Morosinotto <d.morosinotto@icloud.com>
2024-04-17 16:08:20 +0200

    using input trasform + alias withComponentInputBinding

--- src/app/products/product-details/product-details.component.ts
+++ src/app/products/product-details/product-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject, Input } from "@angular/core";
+import { Component, OnInit, computed, inject, input } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 
 import { CartService } from "@app/state/cart.service";
@@ -6,30 +6,26 @@ import { Product, ProductService } from "@app/state/product.service";
 import { NgIf, CurrencyPipe } from "@angular/common";
 
 export const injectPar = (name: string) => {
-	const route = inject(ActivatedRoute);
-	const routeParams = route.snapshot.paramMap;
-	return routeParams.get(name);
+    const route = inject(ActivatedRoute);
+    const routeParams = route.snapshot.paramMap;
+    return routeParams.get(name);
 };
 
 @Component({
-	selector: "app-product-details",
-	templateUrl: "./product-details.component.html",
-	styles: [],
-	standalone: true,
-	imports: [NgIf, CurrencyPipe]
+    selector: "app-product-details",
+    templateUrl: "./product-details.component.html",
+    styles: [],
+    standalone: true,
+    imports: [NgIf, CurrencyPipe],
 })
-export default class ProductDetailsComponent implements OnInit {
-	product: Product | undefined;
-	cartService = inject(CartService);
-	productService = inject(ProductService);
-	@Input() productId!: string; //DEVE MATCHARE IL NOME DEL RouteParams!!
+export default class ProductDetailsComponent {
+    cartService = inject(CartService);
+    productService = inject(ProductService);
+    $pid = input.required<number, string>({ transform: (productId) => +productId, alias: "productId" }); //DEVE MATCHARE IL NOME DEL RouteParams!!
+    $product = computed(() => this.productService.getAll().find((product) => product.id === this.$pid()));
 
-	ngOnInit() {
-		this.product = this.productService.getAll().find(product => product.id === +this.productId);
-	}
-
-	Buy(product: Product) {
-		this.cartService.addToCart(product);
-		window.alert("Your product has been added to the cart!");
-	}
+    Buy(product: Product) {
+        this.cartService.addToCart(product);
+        window.alert("Your product has been added to the cart!");
+    }
 }
